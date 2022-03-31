730c570fa41010683ada59d8c1106095097335ee
Rajat Saxena <hi@rajatsaxena.dev>
2022-03-31 17:28:33 +0530

    Added a vague environment

--- packages/core/lib/config/strings.ts
+++ packages/core/lib/config/strings.ts
@@ -5,4 +5,3 @@ export const SUBSCRIPTION_NOT_VALID = "Subscription not valid";
 export const FILE_IS_REQUIRED = "File is required";
 export const FILE_SIZE_EXCEEDED = "File size exceeded";
 export const NOT_FOUND = "Not found";
-export const VAGUE = "vague";
