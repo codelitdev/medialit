931a03268d345aed8b41a712bba85186ec3bb18d
Ravi Rajput <ravi81rajput@gmail.com>
2024-01-23 15:25:39 +0530

    Fixed the hydration error

--- apps/web/app/dashboard/app/[name]/layout.tsx
+++ apps/web/app/dashboard/app/[name]/layout.tsx
@@ -1,23 +1,11 @@
 import React from "react";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import { NavBar } from "@/components/nav-bar";
 import Link from "next/link";
 
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-    title: "Medialit",
-    description: "",
-};
-
-export default async function RootLayout({
+export default async function FilesLayout({
     params,
-    searchParams,
     children,
 }: {
     params: { name: string };
-    searchParams: { page: string };
     children: React.ReactNode;
 }) {
     const name = params.name;
@@ -25,25 +13,21 @@ export default async function RootLayout({
     // const decodeURIComponentName = decodeURIComponent(name);
     // console.log("decodedName", decodedName, name);
     // console.log("decodeURIComponentName", decodeURIComponentName, name);
-    
+
     return (
-        <html lang="en">
-            <body className={inter.className}>
-                <NavBar />
-                <main className="mx-auto max-w-[1024px] min-h-screen">
-                    <div className="text-primary font-semibold mt-5 px-2">
-                        <Link href="/dashboard">
-                            {" "}
-                            All apps /{" "}
-                            <span className="text-muted-foreground">
-                                {decodedName}
-                            </span>
-                        </Link>
-                    </div>
-                    <div className="px-2 py-8 ">{children}</div>
-                </main>
-                {/* <Footer /> */}
-            </body>
-        </html>
+        <>
+            <main className="mx-auto max-w-[1024px] min-h-screen">
+                <div className="text-primary font-semibold px-2">
+                    <Link href="/dashboard">
+                        {" "}
+                        All apps /{" "}
+                        <span className="text-muted-foreground">
+                            {decodedName}
+                        </span>
+                    </Link>
+                </div>
+                <div className="px-2 py-8 ">{children}</div>
+            </main>
+        </>
     );
 }
