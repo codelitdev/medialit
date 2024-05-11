d79c060c3680eda4b556a149520d9daf7795764f
Rajat Saxena <hi@rajatsaxena.dev>
2024-05-11 19:39:25 +0530

    Added Cloudfront CDN remotes

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -10,6 +10,18 @@ const nextConfig = {
                 protocol: "https",
                 hostname: "medialit-prod.s3.ap-southeast-1.amazonaws.com",
             },
+            {
+                protocol: "https",
+                hostname: "medialit-prod.s3.ap-southeast-1.amazonaws.com",
+            },
+            {
+                protocol: "https",
+                hostname: "dxffh090a1hlp.cloudfront.net",
+            },
+            {
+                protocol: "https",
+                hostname: "d27g932tzd9f7s.cloudfront.net",
+            },
         ],
     },
 };
